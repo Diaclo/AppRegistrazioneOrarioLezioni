ae0f5464387e0a585348884e7b7bc324a01697f9
Matteo Sacco <matteosacco@outlook.it>
2021-02-02 23:21:40 +0100

    navigator 03

--- navigator.py
+++ navigator.py
@@ -2,12 +2,16 @@
 import time
 import requests
 from selenium import webdriver
-from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait     
 from selenium.webdriver.common.by import By     
 from selenium.webdriver.support import expected_conditions as EC
-import Selenium2Library
+
+"""
+from SeleniumLibrary import WebDriver
+from SeleniumLibrary import AlertKeywords
+from SeleniumLibrary import BrowserManagementKeywords
+"""
 
 class loginBot:
    def __init__(self, username, password):
@@ -26,10 +30,7 @@ class loginBot:
       elearning.send_keys(Keys.RETURN)
       time.sleep(5)
       
-      url = bot.current_url()
-      print(url)
-      
-      #bot.get('https://teams.microsoft.com/dl/launcher/launcher.html?url=%2F_%23%2Fl%2Fmeetup-join%2F19%3Ameeting_Y2NmOTBlYzEtN2M2MC00ZGVmLTliOTAtMjI0NTJjN2IyNzk0%40thread.v2%2F0%3Fcontext%3D%257b%2522Tid%2522%253a%2522e99647dc-1b08-454a-bf8c-699181b389ab%2522%252c%2522Oid%2522%253a%2522080683d2-51aa-4842-aa73-291a43203f71%2522%257d%26anon%3Dtrue&type=meetup-join&deeplinkId=5ca1d260-d778-4949-b842-ec516db7edd0&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true')
+      bot.get('https://teams.microsoft.com/dl/launcher/launcher.html?url=%2F_%23%2Fl%2Fmeetup-join%2F19%3Ameeting_Y2NmOTBlYzEtN2M2MC00ZGVmLTliOTAtMjI0NTJjN2IyNzk0%40thread.v2%2F0%3Fcontext%3D%257b%2522Tid%2522%253a%2522e99647dc-1b08-454a-bf8c-699181b389ab%2522%252c%2522Oid%2522%253a%2522080683d2-51aa-4842-aa73-291a43203f71%2522%257d%26anon%3Dtrue&type=meetup-join&deeplinkId=5ca1d260-d778-4949-b842-ec516db7edd0&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true')
       
       """
       join = bot.find_element_by_xpath('/html/body/div/div/div/div[1]/div/div[3]/button[2]')
