9c7c497f20613b9d0061e9559b26e623ef16cbc6
Matteo Sacco <matteosacco@outlook.it>
2021-02-02 17:14:40 +0100

    navigator 02

--- navigator.py
+++ navigator.py
@@ -20,13 +20,21 @@ class loginBot:
    def goToPage(self):
       bot = self.bot
       bot.get('https://www.unibo.it/it/didattica/insegnamenti/insegnamento/2020/461145/orariolezioni')
-      # elearning = bot.find_element(By.XPATH('//*[@id="u-content-large"]/div[2]/div/div/ul/li[1]/p/a')).get_attribute('herf')
-      bot.get(bot.find_element(By.XPATH('//*[@id="u-content-large"]/div[2]/div/div/ul/li[1]/p/a')).get_attribute('herf'))
-
-      join = bot.find_element_by_link_text('Continua in questo browser')
+      elearning = bot.find_element(By.XPATH('/html/body/div[4]/div[3]/div[2]/div[3]/div/div/div[2]/div/div/ul/li[1]/p/a')).get_attribute('herf')
+      elearning = str(elearning)
+      print(elearning)
+      # bot.get(elearning)
+      bot.get('https://teams.microsoft.com/dl/launcher/launcher.html?url=%2F_%23%2Fl%2Fmeetup-join%2F19%3Ameeting_Y2NmOTBlYzEtN2M2MC00ZGVmLTliOTAtMjI0NTJjN2IyNzk0%40thread.v2%2F0%3Fcontext%3D%257b%2522Tid%2522%253a%2522e99647dc-1b08-454a-bf8c-699181b389ab%2522%252c%2522Oid%2522%253a%2522080683d2-51aa-4842-aa73-291a43203f71%2522%257d%26anon%3Dtrue&type=meetup-join&deeplinkId=5ca1d260-d778-4949-b842-ec516db7edd0&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true')
+      
+      join = bot.find_element_by_xpath('/html/body/div/div/div/div[1]/div/div[3]/button[2]')
       join.send_keys(Keys.RETURN)
 
+      bot.switch_to.alert().accept()
+      bot._switch_to.default_content()
 
+
+   # script per login qualora fosse necessario accedere
+   """
    def login(self):
       bot = self.bot
       bot.get('link') # aggiungere il link
@@ -39,6 +47,9 @@ class loginBot:
       password.send_keys(self.password)
       password.send_keys(Keys.RETURN)
       time.sleep(5)
+   """
+
+# Chiamata delle funzioni
 
 log = loginBot('username', 'password')
 log.goToPage()
