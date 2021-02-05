615a4aa23f9b226a70eda57b3780655dd35c6a90
Matteo Sacco <matteosacco@outlook.it>
2021-02-05 12:42:33 +0100

    recorder 02

--- recorder.py
+++ recorder.py
@@ -3,16 +3,15 @@ import cv2
 import numpy
 import pyautogui
 
-class recordVideo(durataVideo):
-   self.durata = durataVideo
-   screenSize = pyautogui.size()
-   outCodec = cv2.VideoWriter_fourcc(*"XVID")
-   fps = 24.0
-   minuto = 1440 # a 24 fps un minuto di video dura 1440 fps
-   ora = 86400 # minuto * 60 -> in fps
-   out = cv2.VideoWriter("output.avi", outCodec, fps, screenSize)
-
-   def __init__(self, durata):
+class recordVideo:
+   def __init__(self, durataVideo):
+      self.durata = durataVideo
+      screenSize = pyautogui.size()
+      outCodec = cv2.VideoWriter_fourcc(*"XVID")
+      fps = 24.0
+      minuto = 1440 # a 24 fps un minuto di video dura 1440 fps
+      ora = 86400 # minuto * 60 -> in fps
+      out = cv2.VideoWriter("output.avi", outCodec, fps, screenSize)
       i = 0
       for i in range( ora * self.durata ):
          img = pyautogui.screenshot()
