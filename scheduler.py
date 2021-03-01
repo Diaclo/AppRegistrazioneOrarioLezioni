06b31de5d39d2069489cc2d76bbafe1c175107b6
alessioarcara <Spammer123@>
2021-03-01 08:50:56 +0100

    DONE: progressi fatti su Scheduler TODO: da finire Scheduler

--- scheduler.py
+++ scheduler.py
@@ -9,11 +9,19 @@ class Scheduler:
     orariOggiFine = None
 
     def __init__(self):
-        self.scrape = WebScraper(self.leggiFile()[f'Corso {1}'][1])
-        # orariOggiInizio = {k for k in scrape.orarioInizio if datetime(2021, 3, 4).date() == k.date()}
-        # orariOggiFine = {k for k in scrape.orarioFine if datetime(2021, 3, 4).date() == k.date()}
-        # print(orariOggiInizio)
-        # print(orariOggiFine)
+        self.sched = BackgroundScheduler(daemon=True)
+
+        corsi = self.leggiFile()
+        for i in corsi:
+            scrape = WebScraper(corsi[i][1])
+            orariOggiInizio = [k for k in scrape.orarioInizio if datetime(2021, 3, 4).date() == k.date()]
+            orariOggiFine = [k for k in scrape.orarioFine if datetime(2021, 3, 4).date() == k.date()]
+            self.sched = BackgroundScheduler(daemon=True)
+
+            for j in orariOggiInizio:
+                print(j)
+                self.sched.add_job(lambda: print('Anthony'), 'interval', seconds=1)
+                self.sched.add_job(lambda: print('Anthony'), 'date', run_date=str(j))
 
     def leggiFile(self):
         with open('pyschedule.csv') as csv_file:
@@ -21,16 +29,12 @@ class Scheduler:
             d = {}
             for k, t, o in reader:
                 d[k] = t, o
+                print(d)
             return d
 
-    def routine(self):
-        orariOggiInizio = [k for k in self.scrape.orarioInizio if datetime(2021, 3, 4).date() == k.date()]
-        self.sched.add_job(lambda: print('Anthony'), 'date', run_date=str(orariOggiInizio[0]))
-        # self.sched.add_job(lambda: print('Anthony'), 'interval', seconds=1)
+    # def routine(self):
 
     def accensione(self):
-        self.sched = BackgroundScheduler(daemon=True)
-        self.routine()
         self.sched.start()
 
     def spegnimento(self):
