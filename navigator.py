4bf25e7e75690197e2704e477cc4d9a8d7a6f7ba
Matteo Sacco <matteosacco@outlook.it>
2021-03-01 10:28:24 +0100

    Update navigator.py

--- navigator.py
+++ navigator.py
@@ -13,21 +13,9 @@ class loginBot:
         self.bot = webdriver.Chrome(desired_capabilities=self.caps)
         self.bot.maximize_window()
 
-    def goToPage(self):
+    def goToPage(self, link):
         bot = self.bot
-        """ # Accesso alla pagina 'Orari Corso'
-      -> non più necessaria, si accede direttamente alla stanza teams
-      bot.get('https://www.unibo.it/it/didattica/insegnamenti/insegnamento/2020/461145/orariolezioni')
-      time.sleep(5)
-      elearning = bot.find_element_by_class_name('elearning a')
-      elearning.send_keys(Keys.RETURN)
-      time.sleep(5)
-      """
-        # bot.get('https://teams.microsoft.com/dl/launcher/launcher.html?url=%2F_%23%2Fl%2Fmeetup-join%2F19%3Ameeting_Y2NmOTBlYzEtN2M2MC00ZGVmLTliOTAtMjI0NTJjN2IyNzk0%40thread.v2%2F0%3Fcontext%3D%257b%2522Tid%2522%253a%2522e99647dc-1b08-454a-bf8c-699181b389ab%2522%252c%2522Oid%2522%253a%2522080683d2-51aa-4842-aa73-291a43203f71%2522%257d%26anon%3Dtrue&type=meetup-join&deeplinkId=5ca1d260-d778-4949-b842-ec516db7edd0&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true')
-        # bot.get('https://teams.microsoft.com/dl/launcher/launcher.html?url=%2F_%23%2Fl%2Fmeetup-join%2F19%3Ameeting_MTZlZmUyMmUtNzdhMi00ZjE2LThmMDMtOGQ3NDc4NjM1NmVk%40thread.v2%2F0%3Fcontext%3D%257b%2522Tid%2522%253a%2522e99647dc-1b08-454a-bf8c-699181b389ab%2522%252c%2522Oid%2522%253a%2522080683d2-51aa-4842-aa73-291a43203f71%2522%257d%26anon%3Dtrue&type=meetup-join&deeplinkId=2832b5cb-1985-4487-a563-e8201c84744c&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true')
-        bot.get(
-            "https://teams.microsoft.com/dl/launcher/launcher.html?url=%2F_%23%2Fl%2Fmeetup-join%2F19%3Ameeting_ZTViYWFmNzUtYWUzMy00N2EwLTlmNGQtZjBjM2Y4Y2RiNzBl%40thread.v2%2F0%3Fcontext%3D%257b%2522Tid%2522%253a%2522e99647dc-1b08-454a-bf8c-699181b389ab%2522%252c%2522Oid%2522%253a%2522080683d2-51aa-4842-aa73-291a43203f71%2522%257d%26anon%3Dtrue&type=meetup-join&deeplinkId=c8f0e1f0-ffa7-4355-8b5e-4a0053c0bc48&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true"
-        )
+        bot.get(link)
         join = bot.find_element_by_xpath(
             "/html/body/div/div/div/div[1]/div/div[3]/button[2]"
         )
@@ -91,14 +79,15 @@ class loginBot:
         time.sleep(5)
 
     def lastjoin(self):
+        bot = self.bot
         attempt = 0
         while attempt == 0:
             try:
-                continueJoin = bot.find_element_by_xpath(
-                    "/html/body/div[5]/div[2]/div/div/div/div[1]/div/div/div[2]/div/button"
+                continueLastJoin = bot.find_element_by_xpath(
+                    "/html/body/div[5]/div[2]/div/div/div/div[1]/div/div/div[2]/div/button/text()"
                 )
-                continueJoin.send_keys(Keys.RETURN)
-                if continueJoin:
+                continueLastJoin.send_keys(Keys.RETURN)
+                if continueLastJoin:
                     attempt = 1
                     print("done!!!")
             except:
@@ -111,8 +100,10 @@ class loginBot:
 
 
 # Chiamata delle funzioni
-log = loginBot("mail", "password")
-log.goToPage()
+log = loginBot("matteo.sacco4@studio.unibo.it", "UniBo/2018")
+log.goToPage(
+    "https://teams.microsoft.com/dl/launcher/launcher.html?url=%2F_%23%2Fl%2Fmeetup-join%2F19%3Ameeting_ZTViYWFmNzUtYWUzMy00N2EwLTlmNGQtZjBjM2Y4Y2RiNzBl%40thread.v2%2F0%3Fcontext%3D%257b%2522Tid%2522%253a%2522e99647dc-1b08-454a-bf8c-699181b389ab%2522%252c%2522Oid%2522%253a%2522080683d2-51aa-4842-aa73-291a43203f71%2522%257d%26anon%3Dtrue&type=meetup-join&deeplinkId=c8f0e1f0-ffa7-4355-8b5e-4a0053c0bc48&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true"
+)
 log.login()
 log.lastjoin()
 time.sleep(10)
